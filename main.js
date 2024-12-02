2428cc96ac83e4637964a34e76a2a4d262f4266e
lili-pv-2370 <yas21092000@gmail.com>
2024-12-01 20:18:13 -0600

    primer cambio

---
+++ main.js
@@ -0,0 +1,15 @@
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+typewriter.typeString('Desarrolladora Frontend Jr.')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('¡Si lo puedes imaginar, lo puedes crear!')
+    .pauseFor(2500)
+    //numero de caracteres que se borraran
+    .deleteChars(6)
+    .typeString('<strong>Programar!</strong>')
+    .pauseFor(2500)
+    .start();
